5684047af063165b5e0dc2649d22354d7aeb8d1c
Jithu-SM <jithusmusic@gmail.com>
2025-08-27 21:20:54 +0530

    updated cors_allowed_origins

--- recruitment_system/recruitment_system/settings.py
+++ recruitment_system/recruitment_system/settings.py
@@ -56,6 +56,11 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
+
+
 ROOT_URLCONF = 'recruitment_system.urls'
 
 TEMPLATES = [
