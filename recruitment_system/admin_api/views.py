772a0ef5c91653c407a4d0b26e249d6c3c846b0e
Jithu-SM <jithusmusic@gmail.com>
2025-09-19 19:57:57 +0530

    added views and urls for admin

--- recruitment_system/admin_api/views.py
+++ recruitment_system/admin_api/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from rest_framework.permissions import IsAdminUser
+from rest_framework.generics import ListAPIView, RetrieveUpdateDestroyAPIView
+from users.models import CustomUser
+from jobs.models import Job
+from resumes.models import Resume
+from applications.models import Application
+from users.serializers import UserSerializer
+from jobs.serializers import JobSerializer
+from resumes.serializers import ResumeSerializer
+from applications.serializers import ApplicationSerializer
 
-# Create your views here.
+# Users
+class AdminUserListView(ListAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAdminUser]
+
+class AdminUserDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAdminUser]
+
+# Jobs
+class AdminJobListView(ListAPIView):
+    queryset = Job.objects.all()
+    serializer_class = JobSerializer
+    permission_classes = [IsAdminUser]
+
+class AdminJobDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = Job.objects.all()
+    serializer_class = JobSerializer
+    permission_classes = [IsAdminUser]
+
+# Resumes
+class AdminResumeListView(ListAPIView):
+    queryset = Resume.objects.all()
+    serializer_class = ResumeSerializer
+    permission_classes = [IsAdminUser]
+
+# Applications
+class AdminApplicationListView(ListAPIView):
+    queryset = Application.objects.all()
+    serializer_class = ApplicationSerializer
+    permission_classes = [IsAdminUser]
